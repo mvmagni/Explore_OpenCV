cbeca1b66a95860c0a03bf66119351afb53c2a94
mvmagni <mv.magni@gmail.com>
2022-09-03 17:27:20 -0400

    Quality of Life and config updates
    Cleaned some code.
    Added different yolo configs and weights to test

--- core/main.py
+++ core/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 import utils
 import yolo_config as yc
+from statistics import mean
+import time
 
 print(f'OpenCV Version: {cv.__version__}')
 print(f'CUDA enabled devices: {cv.cuda.getCudaEnabledDeviceCount()}')
@@ -18,7 +20,6 @@ SHOW_FPS=False
 SHOW_DETECT=False
 SHOW_DETECT_LABELS=True
 
-
 ###############################################################################
 # Get coco info
 classNames = utils.get_classNames(f'{PROJECT_ROOT_DIR}/net_configs/coco.names')
@@ -27,13 +28,62 @@ if classNames is None:
     exit()
 ###############################################################################
 
-
 ###############################################################################
 # Setup the NN parameters
 # Setup the basics for darknet in CV
 # Load in model config and weights
-modelConfiguration=f'{PROJECT_ROOT_DIR}/net_configs/yolov3-320.cfg'
-modelWeights=f'{PROJECT_ROOT_DIR}/net_configs/yolov3-320.weights'
+CONFIG_TYPE=1
+
+if CONFIG_TYPE == 1:
+    # Default target for width and height in YOLO network
+    # Used in cv.dnn.blobFromImage
+    whT = 320
+    hhT = 320
+    modelConfiguration=f'{PROJECT_ROOT_DIR}/net_configs/yolov3-320.cfg'
+    modelWeights=f'{PROJECT_ROOT_DIR}/net_configs/yolov3.weights'
+
+elif CONFIG_TYPE == 2: 
+    # Default target for width and height in YOLO network
+    # Used in cv.dnn.blobFromImage
+    whT = 412
+    hhT = 412
+    modelConfiguration=f'{PROJECT_ROOT_DIR}/net_configs/yolov3-tiny.cfg'
+    modelWeights=f'{PROJECT_ROOT_DIR}/net_configs/yolov3-tiny.weights'
+
+elif CONFIG_TYPE == 3:
+    # Default target for width and height in YOLO network
+    # Used in cv.dnn.blobFromImage
+    whT = 608
+    hhT = 608
+    modelConfiguration=f'{PROJECT_ROOT_DIR}/net_configs/yolov4_new_608.cfg'
+    modelWeights=f'{PROJECT_ROOT_DIR}/net_configs/yolov4_new.weights'
+
+elif CONFIG_TYPE == 4:
+    # Default target for width and height in YOLO network
+    # Used in cv.dnn.blobFromImage
+    whT = 416
+    hhT = 416
+    modelConfiguration=f'{PROJECT_ROOT_DIR}/net_configs/yolov4_new_416.cfg'
+    modelWeights=f'{PROJECT_ROOT_DIR}/net_configs/yolov4_new.weights'
+
+elif CONFIG_TYPE == 5:
+    # Default target for width and height in YOLO network
+    # Used in cv.dnn.blobFromImage
+    whT = 416
+    hhT = 416
+    modelConfiguration=f'{PROJECT_ROOT_DIR}/net_configs/yolov3-416.cfg'
+    modelWeights=f'{PROJECT_ROOT_DIR}/net_configs/yolov3.weights'
+
+if CONFIG_TYPE == 6:
+    # Default target for width and height in YOLO network
+    # Used in cv.dnn.blobFromImage
+    whT = 320
+    hhT = 180
+    modelConfiguration=f'{PROJECT_ROOT_DIR}/net_configs/yolov3-320_180.cfg'
+    modelWeights=f'{PROJECT_ROOT_DIR}/net_configs/yolov3.weights'
+
+
+
 net, outputNames = yc.get_net_config(modelConfiguration, modelWeights)
 ###############################################################################
 
@@ -48,12 +98,6 @@ cap.set(cv.CAP_PROP_FRAME_WIDTH,960)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT,540)
 ################################################################################
 
-
-# Default target for width and height in YOLO network
-# Used in cv.dnn.blobFromImage
-whT = 320
-hhT = 320
-
 fps_queue=None
 prev_frame_time = 0
 frame_counter = 0
@@ -65,16 +109,15 @@ while True:
         print(f'Image size: {frame.shape}')
 
     if SHOW_DETECT:
-        blob = cv.dnn.blobFromImage(frame, 1/255, (whT,whT),[0,0,0],1,crop=False)
+        blob = cv.dnn.blobFromImage(frame, 1/255, (whT,hhT),[0,0,0],1,crop=False)
         net.setInput(blob)
         outputs = net.forward(outputNames)
             
         utils.findObjects(outputs,frame,classNames, show_labels=SHOW_DETECT_LABELS)
 
     if SHOW_FPS:
-        
         prev_frame_time, fps_queue = utils.show_fps(frame, prev_frame_time, fps_queue)
- 
+        
     # Show the image
     cv.imshow('OpenCV Test',frame)
 
